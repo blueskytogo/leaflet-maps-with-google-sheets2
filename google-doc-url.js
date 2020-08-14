59797f1c928403f57bd873e68aed5fff49acd452
blueskytogo <68955765+blueskytogo@users.noreply.github.com>
2020-08-14 21:33:50 +0900

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/10AByMnuYL3N_juwaBdVRf-pEB3Sg1L5JHO8XHLDcr8s/edit#gid=0';
